452616ef7bba4777d739b4e13cb2f52cae852ef2
Jaydip Garala <jaydipkumargarala@gmail.com>
2023-05-22 19:46:40 +0530

    First Time

---
+++ Account/Account/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Account;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Account/Account/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Account;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
